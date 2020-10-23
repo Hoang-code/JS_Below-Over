46a4399e5fe38977d2b4bb507063943c34843088
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-23 20:24:25 +0700

    tinhtong

---
+++ main.js
@@ -0,0 +1,7 @@
+function kiemtratongAB(){
+    let A=Number(document.getElementById("soA").value)
+    let B=Number(document.getElementById("soB").value)
+    let total = A+B
+    
+    alert(total)
+}
