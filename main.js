20421e12316643112cbe60283bc73f571a8906b9
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-23 20:35:21 +0700

    hoanthien

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function kiemtratongAB(){
-    let A=Number(document.getElementById("soA").value)
-    let B=Number(document.getElementById("soB").value)
-    let total = A+B
-    
-    alert(total)
+    let a=Number(document.getElementById("soA").value)
+    let b=Number(document.getElementById("soB").value)
+    let total = a + b
+    if (total < 4){
+        document.getElementById("tong").innerText = "Bellow"
+    } else{
+        document.getElementById("tong").innerText = "Over"
+    }
+   
 }
